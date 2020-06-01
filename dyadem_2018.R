853ce4837db3c1c076c19d22c38389d7f008a72e
sandygraniermana <47325536+sandygraniermana@users.noreply.github.com>
2020-06-01 20:55:18 +0200

    update

--- dyadem_2018.R
+++ dyadem_2018.R
@@ -1,7 +1,5 @@
 
 
-#train(efcs) #taille pop non modifiée
-
 training = train_taille_modif() #taille pop modifiée (1000 cellules)
 
 
@@ -25,18 +23,19 @@ a = efcsALL_FMO[,"popIDscaffold"]
 
 visua = vis(annot,a) # scaffold vs manuelle
 
+#tests de performances
 pre_recE = precision_recall_ann_a_E(ann, a)
 
 pre_rec = precision_recall(ann, a)
 
 pre_rec2 = precision_recall_ann_annot(ann, annot)
 
-
+#score F1
 ref.vec = annot
 run.vec = ann
-
 F1 = Compute.F1(ref.vec, run.vec)
 
+#tests de performances
 prcf = pre_rec_cellAnnot_F1_ann_annot(ann, annot)
 
 prcf2 = pre_rec_cellAnnot_ann_a(ann,a)
@@ -46,5 +45,6 @@ prcf3 = pre_rec_cellAnnot_ann_annot(ann,annot)
 
 prcf4 = pre_rec_cellAnnot_a_annot(a,annot)
 
+#moyenne des tests
 c = colMeans(prcf3[-11,4:5])
 c1 = colMeans(prcf4[-11,4:5])
